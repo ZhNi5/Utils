d9a00abade1dee702a9405c120727f03cb74e063
zvu <zvu@agsr.by>
2023-10-11 14:02:58 +0300

    refactor class Utils.java with method checkDayAndMonth

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,15 @@ public class Main {
         Utils.check(0,0,3);
         System.out.println("0,0,0");
         Utils.check(0,0,0);
+        System.out.println("28,2");
+        Utils.checkDayAndMonth(28,2);
+
+        System.out.println("29,2");
+        Utils.checkDayAndMonth(29,2);
+        System.out.println("30,2");
+        Utils.checkDayAndMonth(30,2);
+        System.out.println("1,1");
+        Utils.checkDayAndMonth(1,1);
+
     }
 }
--- src/Utils.java
+++ src/Utils.java
@@ -20,4 +20,59 @@ public class Utils {
         }
     }
 
+    public static void checkDayAndMonth(int day,int month){
+       if ((day >= 1) && (day <= 31) && (1 <= month) && (month <= 12)) {
+
+           switch (month) {
+               case 1:
+                   if((day==1)||(day==7)) {
+                       System.out.println("It is Holiday");
+                   }
+                   break;
+               case 2:
+                   if(day>29) {
+                       System.out.println("The date is unavaliable");
+                   }
+                   else
+                   {
+                       System.out.println("It is not a Holiday");
+                   }
+                   break;
+               case 3:
+                   if(day==8) {
+                       System.out.println("It is Holiday");
+                   }
+                   break;
+               case 5:
+                   if((day==1)||(day==9)) {
+                       System.out.println("It is Holiday");
+                   }
+                   break;
+               case 6:
+                   if(day==3) {
+                       System.out.println("It is Holiday");
+                   }
+                   break;
+               case 11:
+                   if(day==7) {
+                       System.out.println("It is Holiday");
+                   }
+                   break;
+               case 12:
+                   if(day==25) {
+                       System.out.println("It is Holiday");
+                   }
+                   break;
+               default:
+                   System.out.println("It is not a Holiday");
+                   break;
+           }
+       }
+       else
+       {
+           System.out.println("The date is unavaliable");
+       }
+
+    }
+
 }
