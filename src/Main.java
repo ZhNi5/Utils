73102edbe26f1f313d7c9bf92c0714a6df897780
zvu <zvu@agsr.by>
2023-10-11 12:09:10 +0300

    create class Utils with method check

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("1,2,3");
+        Utils.check(1,2,3);
+        System.out.println("0,2,3");
+        Utils.check(0,2,3);
+        System.out.println("0,0,3");
+        Utils.check(0,0,3);
+        System.out.println("0,0,0");
+        Utils.check(0,0,0);
+    }
+}
